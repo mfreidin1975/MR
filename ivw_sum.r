65962eaf1af041276492d2990b042b2a4f0aac56
mfreidin1975 <36857885+mfreidin1975@users.noreply.github.com>
2018-02-26 17:16:51 +0000

    Create ivw_sum.r

---
+++ ivw_sum.r
@@ -0,0 +1,23 @@
+#### This is a simple MR for a single SNP using ivw
+
+ivw_sum<-function(B_o,SE_o,B_e,SE_e,rho=0) {
+  B<-B_o/B_e
+  if (rho==0) {
+     print("two-sample design, simple SE")
+     SE<-SE_o/B_e
+     }
+    else {
+     print("single-sample desing")
+     SE<-sqrt(SE_o^2/B_e^2+B_o^2*SE_e^2/B_e^4-2*rho*B_o*SE_e*SE_o/B_e^3)
+     }
+ Z<-B/SE
+ p<-2*pnorm(-abs(Z))
+ OR<-exp(B)
+ CI_L<-exp(B-1.96*SE)
+ CI_U<-exp(B+1.96*SE)
+ res<-cbind(B,SE,OR,CI_L,CI_U,p)
+ names(res)<-c("Effect","SE","OR","CI_L","CI_U","P-value")
+ 
+ return(res)
+ }
+ 
